1b2b1966601133b87c85eb0897072918dd21b2eb
Wisdom <eluewisdom@gmail.com>
2023-08-12 22:13:30 +0100

    add account status verification in news routes

--- dist/routes/news/news.routes.js
+++ dist/routes/news/news.routes.js
@@ -7,11 +7,15 @@ const get_single_news_controller_1 = require("../../controllers/news/get_single_
 const update_news_controller_1 = require("../../controllers/news/update_news/update.news.controller");
 const delete_news_controller_1 = require("../../controllers/news/delete_news/delete.news.controller");
 const auth_middleware_1 = require("../../middleware/auth.middleware");
+const account_status_1 = require("../../middleware/account.status");
 const router = (0, express_1.Router)();
-router.route("/").get(get_all_news_controller_1.getAllNews).post(auth_middleware_1.verifyAuth, add_news_controller_1.addNews);
+router
+    .route("/")
+    .get(get_all_news_controller_1.getAllNews)
+    .post(auth_middleware_1.verifyAuth, account_status_1.verifyAccountStatus, add_news_controller_1.addNews);
 router
     .route("/:slug/:newsId")
     .get(get_single_news_controller_1.getSingleNews)
-    .put(auth_middleware_1.verifyAuth, update_news_controller_1.updateNews)
-    .delete(auth_middleware_1.verifyAuth, auth_middleware_1.verifyAdmin, delete_news_controller_1.deleteNews);
+    .put(auth_middleware_1.verifyAuth, account_status_1.verifyAccountStatus, update_news_controller_1.updateNews)
+    .delete(auth_middleware_1.verifyAuth, auth_middleware_1.verifyAdmin, account_status_1.verifyAccountStatus, delete_news_controller_1.deleteNews);
 exports.default = router;
--- server/routes/news/news.routes.ts
+++ server/routes/news/news.routes.ts
@@ -5,14 +5,18 @@ import { getSingleNews } from "../../controllers/news/get_single_news/get.single
 import { updateNews } from "../../controllers/news/update_news/update.news.controller";
 import { deleteNews } from "../../controllers/news/delete_news/delete.news.controller";
 import { verifyAdmin, verifyAuth } from "../../middleware/auth.middleware";
+import { verifyAccountStatus } from "../../middleware/account.status";
 
 const router = Router();
 
-router.route("/").get(getAllNews).post(verifyAuth, addNews);
+router
+  .route("/")
+  .get(getAllNews)
+  .post(verifyAuth, verifyAccountStatus, addNews);
 router
   .route("/:slug/:newsId")
   .get(getSingleNews)
-  .put(verifyAuth, updateNews)
-  .delete(verifyAuth, verifyAdmin, deleteNews);
+  .put(verifyAuth, verifyAccountStatus, updateNews)
+  .delete(verifyAuth, verifyAdmin, verifyAccountStatus, deleteNews);
 
 export default router;
