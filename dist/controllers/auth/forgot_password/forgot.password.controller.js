5667985f3b97292063c00a6ff19575cfdce44bba
Wisdom <eluewisdom@gmail.com>
2023-08-12 16:32:18 +0100

    feat: forgot password api donne

--- dist/controllers/auth/forgot_password/forgot.password.controller.js
+++ dist/controllers/auth/forgot_password/forgot.password.controller.js
@@ -14,6 +14,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.forgotPassword = void 0;
 const async_handler_1 = __importDefault(require("../../../helpers/async.handler"));
+const global_error_1 = require("../../../helpers/global.error");
+const prisma_client_1 = __importDefault(require("../../../lib/prisma.client"));
+const crypto_1 = require("crypto");
 exports.forgotPassword = (0, async_handler_1.default)(function (req, res, next) {
-    return __awaiter(this, void 0, void 0, function* () { });
+    return __awaiter(this, void 0, void 0, function* () {
+        const { email } = req.body;
+        if (!email)
+            return next(new global_error_1.AppError("Please provide the email associated with your account", 400));
+        const user = yield prisma_client_1.default.user.findFirst({ where: { email } });
+        if (!user)
+            return next(new global_error_1.AppError("The email provided is not registered", 404));
+        const resetToken = (0, crypto_1.randomBytes)(32).toString("hex") + user.id;
+        const hashedToken = (0, crypto_1.createHash)("sha256").update(resetToken).digest("hex");
+        yield prisma_client_1.default.token.create({
+            data: {
+                token: hashedToken,
+                userId: user.id,
+                expiresAt: new Date(Date.now() + 10 * 60 * 1000),
+            },
+        });
+        const resetUrl = `${process.env.CLIENT_URL}/auth/reset-password/${resetToken}`;
+        const subject = "Password Reset Request";
+        const send_to = email;
+        const sent_from = process.env.EMAIL_USER;
+        const reply_to = process.env.REPLY_TO;
+        // const body = passwordResetEmail({
+        //   username: user.firstName,
+        //   url: resetUrl,
+        //   withGoogle: user.withGoogle,
+        // });
+        try {
+            // sendEmail({ subject, body, send_to, sent_from, reply_to });
+            res.status(200).json({
+                status: "success",
+                token: hashedToken,
+                message: `An email has been sent to ${email} with instructions
+        to reset your password. Please ensure to check your spam folder, Click on 'Report as not spam' so you can keep getting our emails in your inbox`,
+            });
+        }
+        catch (error) {
+            res.status(500).json({
+                status: "fail",
+                message: `Email not sent. Please try again.`,
+            });
+        }
+    });
 });
--- server/controllers/auth/forgot_password/forgot.password.controller.ts
+++ server/controllers/auth/forgot_password/forgot.password.controller.ts
@@ -1,8 +1,60 @@
 import { NextFunction, Request, Response } from "express";
 import handleAsync from "../../../helpers/async.handler";
+import { AppError } from "../../../helpers/global.error";
+import prisma from "../../../lib/prisma.client";
+import { User } from "../../../models/types/user";
+import { createHash, randomBytes } from "crypto";
 
 export const forgotPassword = handleAsync(async function (
   req: Request,
   res: Response,
   next: NextFunction
-) {});
+) {
+  const { email } = req.body;
+  if (!email)
+    return next(
+      new AppError("Please provide the email associated with your account", 400)
+    );
+
+  const user: User | null = await prisma.user.findFirst({ where: { email } });
+  if (!user)
+    return next(new AppError("The email provided is not registered", 404));
+
+  const resetToken = randomBytes(32).toString("hex") + user.id;
+  const hashedToken = createHash("sha256").update(resetToken).digest("hex");
+
+  await prisma.token.create({
+    data: {
+      token: hashedToken,
+      userId: user.id,
+      expiresAt: new Date(Date.now() + 10 * 60 * 1000),
+    },
+  });
+
+  const resetUrl = `${process.env.CLIENT_URL}/auth/reset-password/${resetToken}`;
+
+  const subject = "Password Reset Request";
+  const send_to = email;
+  const sent_from = process.env.EMAIL_USER as string;
+  const reply_to = process.env.REPLY_TO as string;
+  // const body = passwordResetEmail({
+  //   username: user.firstName,
+  //   url: resetUrl,
+  //   withGoogle: user.withGoogle,
+  // });
+
+  try {
+    // sendEmail({ subject, body, send_to, sent_from, reply_to });
+    res.status(200).json({
+      status: "success",
+      token: hashedToken,
+      message: `An email has been sent to ${email} with instructions
+        to reset your password. Please ensure to check your spam folder, Click on 'Report as not spam' so you can keep getting our emails in your inbox`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: `Email not sent. Please try again.`,
+    });
+  }
+});
