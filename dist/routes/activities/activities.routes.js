872ed9c49fc1c2b0c4610dabf2caae1e3656ad10
Wisdom <eluewisdom@gmail.com>
2023-08-23 22:24:06 +0100

    patch: make activities api route protected

--- dist/routes/activities/activities.routes.js
+++ dist/routes/activities/activities.routes.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const get_user_activities_1 = require("../../controllers/activities/get.user.activities");
+const auth_middleware_1 = require("../../middleware/auth.middleware");
 const router = (0, express_1.Router)();
-router.get("/", get_user_activities_1.getUserActivities);
+router.get("/", auth_middleware_1.verifyAuth, get_user_activities_1.getUserActivities);
 exports.default = router;
--- server/routes/activities/activities.routes.ts
+++ server/routes/activities/activities.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getUserActivities } from "../../controllers/activities/get.user.activities";
+import { verifyAuth } from "../../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/", getUserActivities);
+router.get("/", verifyAuth, getUserActivities);
 
 export default router;
